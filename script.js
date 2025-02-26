260f4da2e4b78779fcaef81a4ed1ba401063a7db
Kaushik Korlapati <kaushikkorlapati@Kaushiks-MacBook-Pro.local>
2025-02-26 13:07:23 -0500

    Updating for final submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchQuote').addEventListener('click', async () => {
-    const response = await fetch('https://anapioficeandfire.com/api/characters/581');
+    const response = await fetch('https://anapioficeandfire.com/api/characters/16');
     const data = await response.json();
     document.getElementById('quoteText').innerText = `Character: ${data.name}\nCulture: ${data.culture}`;
     document.getElementById('quoteDialog').showModal();
